dd90aa08780e05a049c3720ed329916f99d788dc
Rolf <rolfschellenberger@gmail.com>
2022-01-13 21:26:44 +0100

    Day 6

--- src/main/kotlin/com/rolf/day06/Day06.kt
+++ src/main/kotlin/com/rolf/day06/Day06.kt
@@ -1,6 +1,8 @@
 package com.rolf.day06
 
 import com.rolf.Day
+import com.rolf.util.MatrixInt
+import com.rolf.util.Point
 
 fun main() {
     Day06().run()
@@ -8,10 +10,57 @@ fun main() {
 
 class Day06 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val locations = lines.map { parseLocation(it) }
+        val grid = MatrixInt.buildDefault(500, 500, -1)
+
+        // Calculate distance from every grid location to every location given.
+        // Set the id when 1 is the closest. Set a -1 when multiple are.
+        for (point in grid.allPoints()) {
+            val closestLocation = locations
+                .mapIndexed { index, location -> index to location.distance(point) }
+                .groupBy { it.second }
+                .minByOrNull { it.key }!!
+            if (closestLocation.value.size == 1) {
+                grid.set(point, closestLocation.value.first().first)
+            }
+        }
+
+        // Find the numbers round the edges and remove them.
+        val edges = grid.getTopEdge() + grid.getLeftEdge() + grid.getRightEdge() + grid.getBottomEdge()
+        val toInclude = locations.indices - edges - listOf(-1)
+
+        // Count the positions for each id
+        val maxArea = grid.allElements()
+            .filter { toInclude.contains(it) }
+            .groupingBy { it }
+            .eachCount()
+            // Return the one with the highest count
+            .maxByOrNull { it.value }!!
+        println(maxArea.value)
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        val locations = lines.map { parseLocation(it) }
+        val grid = MatrixInt.buildDefault(500, 500, 0)
+
+        // Calculate distance from every grid location to every location given.
+        // Set a 1 when it's less than 10000 for all locations.
+        for (point in grid.allPoints()) {
+            val sumLocationDistance = locations
+                .map { it.distance(point) }
+                .sum()
+            if (sumLocationDistance < 10000) {
+                grid.set(point, 1)
+            }
+        }
+
+        // Count the ones
+        println(grid.count(1))
+        // 250000 too high
+    }
+
+    private fun parseLocation(line: String): Point {
+        val (x, y) = line.split(", ").map { it.toInt() }
+        return Point(x, y)
     }
 }
