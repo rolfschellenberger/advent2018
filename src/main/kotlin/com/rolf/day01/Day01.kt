411428881d86d708bc5b6cd29e33decfb247f145
Rolf <rolfschellenberger@gmail.com>
2022-01-13 13:07:41 +0100

    Day 1

--- src/main/kotlin/com/rolf/day01/Day01.kt
+++ src/main/kotlin/com/rolf/day01/Day01.kt
@@ -8,11 +8,23 @@ fun main() {
 
 class Day01 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val numbers = lines.map { it.toInt() }
+        println(numbers.sum())
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        var frequency = 0
+        val frequencies = mutableSetOf(frequency)
+        val numbers = lines.map { it.toInt() }
+        frequencies.add(frequency)
+        while (true) {
+            for (number in numbers) {
+                frequency += number
+                if (!frequencies.add(frequency)) {
+                    println(frequency)
+                    return
+                }
+            }
+        }
     }
-
 }
