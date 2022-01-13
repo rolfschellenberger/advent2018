0457df36c8be3c2c29b1bde402dcd138978425fa
Rolf <rolfschellenberger@gmail.com>
2022-01-13 19:46:40 +0100

    Day 5

--- src/main/kotlin/com/rolf/day05/Day05.kt
+++ src/main/kotlin/com/rolf/day05/Day05.kt
@@ -1,6 +1,7 @@
 package com.rolf.day05
 
 import com.rolf.Day
+import java.util.*
 
 fun main() {
     Day05().run()
@@ -8,10 +9,33 @@ fun main() {
 
 class Day05 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        println(clean(lines.first()).size)
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        val line = lines.first()
+        val lowerChars = line.map { it.lowercaseChar() }
+            .toSet()
+
+        println(lowerChars
+            .map { line.replace(it.toString(), "").replace(it.uppercase(), "") }
+            .map { clean(it).size }
+            .minOrNull())
+    }
+
+    private fun clean(line: String): Stack<Char> {
+        val stack = Stack<Char>()
+        for (char in line) {
+            if (stack.isNotEmpty() && isOpposites(stack.peek(), char)) {
+                stack.pop()
+            } else {
+                stack.push(char)
+            }
+        }
+        return stack
+    }
+
+    private fun isOpposites(a: Char, b: Char): Boolean {
+        return a != b && (a.uppercaseChar() == b || a == b.uppercaseChar())
     }
 }
