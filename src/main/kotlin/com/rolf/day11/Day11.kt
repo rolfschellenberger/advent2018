1b0a55ee5bac749d1f69a56a1c4cd050f728b42e
Rolf <rolfschellenberger@gmail.com>
2022-01-15 12:11:09 +0100

    Day 11

--- src/main/kotlin/com/rolf/day11/Day11.kt
+++ src/main/kotlin/com/rolf/day11/Day11.kt
@@ -1,6 +1,8 @@
 package com.rolf.day11
 
 import com.rolf.Day
+import com.rolf.util.MatrixInt
+import com.rolf.util.Point
 
 fun main() {
     Day11().run()
@@ -8,10 +10,80 @@ fun main() {
 
 class Day11 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val serialNumber = lines.first().toInt()
+        val grid = MatrixInt.buildDefault(300, 300, 0)
+        for (point in grid.allPoints()) {
+            val powerLevel = calculatePowerLevel(point, serialNumber)
+            grid.set(point, powerLevel)
+        }
+
+        // Find the largest power cell of a 3x3 size
+        val size = 3
+        val (position, _) = findMaxPower(grid, size)
+        println("${position.x},${position.y}")
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        val serialNumber = lines.first().toInt()
+        val grid = MatrixInt.buildDefault(300, 300, 0)
+        for (point in grid.allPoints()) {
+            val powerLevel = calculatePowerLevel(point, serialNumber)
+            grid.set(point, powerLevel)
+        }
+
+        // Find the largest power cell of a NxN size
+        var maxPower = Int.MIN_VALUE
+        var bestPosition = Point(0, 0)
+        var bestSize = 0
+        for (size in 1..300) {
+            val (position, power) = findMaxPower(grid, size)
+            if (power > maxPower) {
+                maxPower = power
+                bestPosition = position
+                bestSize = size
+            }
+        }
+        println("${bestPosition.x},${bestPosition.y},$bestSize")
+    }
+
+    private fun findMaxPower(grid: MatrixInt, size: Int): Pair<Point, Int> {
+        var maxPower = Int.MIN_VALUE
+        var position = Point(0, 0)
+        for (y in 0 until grid.height() - size) {
+            for (x in 0 until grid.width() - size) {
+                val totalPower = sumCells(grid, x, y, size)
+                if (totalPower > maxPower) {
+                    maxPower = totalPower
+                    position = Point(x + 1, y + 1)
+                }
+            }
+        }
+        return position to maxPower
+    }
+
+    // TODO: Can we make a function for this to make a selection of a box from x,y
+    private fun sumCells(grid: MatrixInt, startX: Int, startY: Int, size: Int): Int {
+        var sum = 0
+        for (y in startY until startY + size) {
+            for (x in startX until startX + size) {
+                sum += grid.get(x, y)
+            }
+        }
+        return sum
+    }
+
+    private fun calculatePowerLevel(point: Point, serialNumber: Int): Int {
+        // Remember to increase x and y with 1
+        val x = point.x + 1
+        val y = point.y + 1
+        val rackId = x + 10
+        var powerLevel = rackId * y
+        powerLevel += serialNumber
+        powerLevel *= rackId
+        val chars = powerLevel.toString().toCharArray()
+        if (chars.size >= 3) {
+            return chars[chars.size - 3].toString().toInt() - 5
+        }
+        return 0
     }
 }
