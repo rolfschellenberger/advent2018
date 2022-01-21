31996521018c3ab3bfd2ccc3563e70dc5c664533
Rolf <rolfschellenberger@gmail.com>
2022-01-21 21:35:25 +0100

    Day 24

--- src/main/kotlin/com/rolf/day24/Day24.kt
+++ src/main/kotlin/com/rolf/day24/Day24.kt
@@ -9,17 +9,27 @@ fun main() {
 
 class Day24 : Day() {
     override fun solve1(lines: List<String>) {
+        val (system1, system2) = fight(lines)
+        println((system1 + system2).map { it.units }.sum())
+    }
+
+    override fun solve2(lines: List<String>) {
+        var immuneWin = false
+        var boost = 1
+        var lastSum = 0
+        while (!immuneWin) {
+            val (system1, system2) = fight(lines, boost)
+            lastSum = (system1 + system2).map { it.units }.sum()
+            immuneWin = system1.any { !it.isDead() } && system2.all { it.isDead() }
+            boost++
+        }
+        println(lastSum)
+    }
+
+    private fun fight(lines: List<String>, boost: Int = 0): Pair<List<Group>, List<Group>> {
         val (group1, group2) = groupLines(lines, "")
-        val system1 = parseSystem(group1)
+        val system1 = parseSystem(group1, boost)
         val system2 = parseSystem(group2)
-//        val system1 = listOf(
-//            Group(17, 5390, emptySet(), setOf("radiation", "bludgeoning"), "fire", 4507, 2),
-//            Group(989, 1274, setOf("fire"), setOf("bludgeoning", "slashing"), "slashing", 25, 3)
-//        )
-//        val system2 = listOf(
-//            Group(801, 4706, emptySet(), setOf("radiation"), "bludgeoning", 116, 1),
-//            Group(4485, 2961, setOf("radiation"), setOf("fire", "cold"), "slashing", 12, 4)
-//        )
 
         while (system1.any { it.canAttack() } && system2.any { it.canAttack() }) {
             // Target selection
@@ -27,23 +37,28 @@ class Day24 : Day() {
             val targets2: Map<Group, Group> = selectTargets(system2, system1)
 
             // Attack
+            val unitsBefore = (system1 + system2).map { it.units }.sum()
             for (group in (targets1 + targets2).toSortedMap()) {
                 val attacker = group.key
                 val defender = group.value
                 val damage = attacker.calculateDamageTo(defender)
-                val before = defender.units
                 defender.takeDamage(damage)
             }
+            val unitsAfter = (system1 + system2).map { it.units }.sum()
+
+            // This means the fight never ends
+            if (unitsAfter == unitsBefore) {
+                return Pair(system1, system2)
+            }
         }
-
-        println((system1 + system2).map { it.units }.sum())
+        return Pair(system1, system2)
     }
 
-    private fun parseSystem(lines: List<String>): List<Group> {
-        return lines.subList(1, lines.size).map { parseGroup(it) }
+    private fun parseSystem(lines: List<String>, boost: Int = 0): List<Group> {
+        return lines.subList(1, lines.size).map { parseGroup(it, boost) }
     }
 
-    private fun parseGroup(line: String): Group {
+    private fun parseGroup(line: String, boost: Int): Group {
         // 4081 units each with 8009 hit points (immune to slashing, radiation; weak to bludgeoning, cold) with an attack that does 17 fire damage at initiative 7
         // 2599 units each with 11625 hit points with an attack that does 36 bludgeoning damage at initiative 17
         // 4232 units each with 4848 hit points (weak to slashing) with an attack that does 11 bludgeoning damage at initiative 13
@@ -73,7 +88,7 @@ class Day24 : Day() {
         val parts = default.split(" ")
         val units = parts[0].toInt()
         val hitPoints = parts[4].toInt()
-        val attackPoints = parts[12].toInt()
+        val attackPoints = parts[12].toInt() + boost
         val attackType = parts[13]
         val initiative = parts[17].toInt()
 
@@ -98,7 +113,7 @@ class Day24 : Day() {
             .filter { it.canAttack() }
             .sortedWith(
                 compareByDescending<Group> { it.effectivePower }
-                    .thenBy { it.initiative }
+                    .thenByDescending { it.initiative }
             )
     }
 
@@ -112,12 +127,9 @@ class Day24 : Day() {
             ?.value
             ?.sortedWith(
                 compareByDescending<Group> { it.effectivePower }
-                    .thenBy { it.initiative }
+                    .thenByDescending { it.initiative }
             )
             ?.firstOrNull()
-    }
-
-    override fun solve2(lines: List<String>) {
     }
 }
 
